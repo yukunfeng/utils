e469c82609dd422ed13c57547dd4fd4d7801e3cf
yukunfeng <yukunfg@gmail.com>
2018-07-24 11:48:48 +0900

    add t-sne

--- t-sne.py
+++ t-sne.py
@@ -4,40 +4,44 @@ Author      : Yukun Feng
 Date        : 2018/07/23
 Email       : yukunfg@gmail.com
 Description : T-SNE to visualize word vectors
-(codes are from some posts in the Internet)
-https://medium.com/@aneesha/using-tsne-to-plot-a-subset-of-similar-words-from-word2vec-bb8eeaea6229
 """
 
-import sys
-import codecs
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+import word2vec
 
 
-def main():
-    embeddings_file = sys.argv[1]
-    wv, vocabulary = load_embeddings(embeddings_file)
-    tsne = TSNE(n_components=2, random_state=0)
-    np.set_printoptions(suppress=True)
-    Y = tsne.fit_transform(wv[:1000, :])
+class TSNEWrapper(object):
+    """TSNE to visualize vectors"""
+    def __init__(self, file_name):
+        """init"""
+        self.model = word2vec.load(file_name)
 
-    plt.scatter(Y[:, 0], Y[:, 1])
-    for label, x, y in zip(vocabulary, Y[:, 0], Y[:, 1]):
-        plt.annotate(
-            label, xy=(x, y),
-            xytext=(0, 0), textcoords='offset points'
-        )
-    plt.show()
+    def show_n_vectors(self, number):
+        """show first n vectors in the word2vec model"""
+        vectors = self.model.vectors[0:number]
+        indexs = np.arange(0, number, dtype=int)
+        words = self.model.vocab[indexs]
+        self.show(vectors, words)
 
+    def show(self, vectors, words):
+        """show
+        """
+        tsne = TSNE(n_components=2, random_state=0)
+        np.set_printoptions(suppress=True)
+        Y = tsne.fit_transform(vectors)
 
-def load_embeddings(file_name):
-    "load embeddings from file"
-    with codecs.open(file_name, 'r', 'utf-8') as f_in:
-        vocabulary, wv = zip(*[line.strip().split(' ', 1) for line in f_in])
-    wv = np.loadtxt(wv)
-    return wv, vocabulary
-
+        plt.scatter(Y[:, 0], Y[:, 1])
+        for label, x, y in zip(words, Y[:, 0], Y[:, 1]):
+            plt.annotate(
+                label, xy=(x, y),
+                xytext=(0, 0), textcoords='offset points'
+            )
+        plt.show()
+        
 
 if __name__ == '__main__':
-    main()
+    # Unit test
+    tnse = TSNEWrapper("../common_corpus/text8.bin")
+    tnse.show_n_vectors(1000)
