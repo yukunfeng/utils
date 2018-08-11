71669ac566061c8825bd3e8f82addb2f902d5a89
yukun <yukunfg@gmail.com>
2018-08-11 12:57:42 +0900

    add load emb

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ Description : Misc utils
 
 import logging
 import torch
+import numpy as np
 
 
 def get_logger(log_file=None):
@@ -32,19 +33,37 @@ def get_logger(log_file=None):
     return logger
 
 
-def word_ids_to_sentence(id_tensor, vocab):
+def word_ids_to_sentence(id_tensor, vocab, word_len=20):
     """Converts a sequence of word ids to a sentence
     id_tensor: torch-based tensor
     vocab: torchtext vocab
     """
-    id_tensor = id_tensor.transpose(0, 1)
-    symbols = []
+    all_strings = ""
     for row in id_tensor:
-        row_symbols = []
+        row_string = ""
         for col in row:
-            row_symbols.append(vocab.itos[col])
-        symbols.append(row_symbols)
-    return symbols
+            word = vocab.itos[col][0:word_len]
+            word = word.ljust(word_len)
+            row_string += word + " "
+        all_strings += row_string + "\n"
+    return all_strings
+
+
+def probability_lookup(id_tensor, field, most_n_word=30):
+    """
+    probability_lookup, id_tensor are logits before softmax
+    """
+    softmax = torch.nn.Softmax(dim=0)
+    probabilities = softmax(id_tensor)[0: most_n_word]
+    numbers, indexs = id_tensor.sort(descending=True)
+    probabilities = softmax(numbers)[0: most_n_word]
+    numbers = numbers[0: most_n_word]
+    indexs = indexs[0: most_n_word]
+    word_list = []
+    for index in indexs:
+        word = field.vocab.itos[index]
+        word_list.append(word)
+    return numbers, probabilities, word_list, indexs
 
 
 def save_word_embedding(vocab, emb, file_name):
@@ -56,6 +75,20 @@ def save_word_embedding(vocab, emb, file_name):
             line = word + " " + " ".join(str_vec) + "\n"
             fh.write(line)
 
+def load_word_embedding(file_path, kind="no_head"):
+    embeddings = []
+    with open(file_path, 'r') as fh:
+        if kind == "no_head":
+            next(fh)
+        for line in fh:
+            line = line.strip()
+            # Skip empty lines
+            if line == "":
+                continue
+            items = line.split()
+            embedding = [float(val) for val in items[1:]]
+            embeddings.append(embedding)
+    return torch.tensor(embeddings)
 
 def save_word_embedding_test():
     vocab = ["a", "b", "c"]
@@ -63,6 +96,12 @@ def save_word_embedding_test():
     save_word_embedding(vocab, emb, "vec.txt")
 
 
+def load_word_embedding_test():
+    emb = load_word_embedding("tmp.vec")
+    print(emb)
+
+
 if __name__ == "__main__":
     # Unit test
-    save_word_embedding_test()
+    #  save_word_embedding_test()
+    load_word_embedding_test()
